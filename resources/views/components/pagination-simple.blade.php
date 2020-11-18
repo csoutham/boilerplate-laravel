53311b55c71ab37e7ddfb4840b15e9955b4afab6
Chris Southam <me@csoutham.com>
2020-11-18 16:56:04 +0000

    Crud generator changes

--- resources/views/components/pagination-simple.blade.php
+++ resources/views/components/pagination-simple.blade.php
@@ -1,37 +1,37 @@
 @if ($paginator->hasPages())
     <nav role="navigation" aria-label="Pagination Navigation">
-        <ul class="flex justify-center text-sm gap-4">
+        <ul class="flex flex-row justify-center text-sm">
             {{-- Previous Page Link --}}
             @if ($paginator->onFirstPage())
-                <li aria-label="@lang('pagination.previous')" class="self-center">
-                    <x-fas-chevron-left class="w-5 h-5 text-gray-600" />
+                <li aria-label="@lang('pagination.previous')" class="self-center w-8 text-center">
+                    <x-heroicon-o-arrow-left class="w-4 h-4 text-gray-400" />
                 </li>
             @else
-                <li class="self-center">
+                <li class="self-center w-8 text-center">
                     <a href="#"
                        wire:click.prevent="previousPage"
                        rel="prev"
                        class="self-center"
                        aria-label="@lang('pagination.previous')">
-                        <x-fas-chevron-left class="w-5 h-5 text-gray-600" />
+                        <x-heroicon-o-arrow-left class="w-4 h-4 text-gray-600" />
                     </a>
                 </li>
             @endif
 
             {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
-                <li class="self-center">
+                <li class="self-center w-8 text-center">
                     <a href="#"
                        wire:click.prevent="nextPage"
                        rel="next"
                        class="self-center"
                        aria-label="@lang('pagination.next')">
-                        <x-fas-chevron-right class="w-5 h-5 text-gray-600" />
+                        <x-heroicon-o-arrow-right class="w-4 h-4 text-gray-600" />
                     </a>
                 </li>
             @else
-                <li aria-disabled="true" aria-label="@lang('pagination.next')" class="self-center">
-                    <x-fas-chevron-right class="w-5 h-5 text-gray-600" />
+                <li aria-disabled="true" aria-label="@lang('pagination.next')" class="self-center w-8 text-center">
+                    <x-heroicon-o-arrow-right class="w-4 h-4 text-gray-400" />
                 </li>
             @endif
         </ul>
--- resources/views/components/pagination.blade.php
+++ resources/views/components/pagination.blade.php
@@ -1,18 +1,22 @@
 @if ($paginator->hasPages())
     <nav role="navigation" aria-label="Pagination Navigation">
-        <ul class="flex justify-center text-sm">
+        <ul class="flex flex-row justify-center text-sm">
             {{-- Previous Page Link --}}
             @if ($paginator->onFirstPage())
-                <li aria-label="@lang('pagination.previous')">
-                    <span class="block px-3 py-2 text-sm font-semibold text-gray-500 border border-r-0 border-gray-300 rounded-l" aria-hidden="true">&larr;</span>
+                <li class="flex px-3 py-2 text-sm font-semibold text-gray-500 border border-gray-300 rounded-l" aria-disabled="true" aria-label="@lang('pagination.previous')">
+                    <span class="self-center" aria-hidden="true">
+                        <x-heroicon-o-arrow-left class="w-4 h-4 text-gray-400" />
+                    </span>
                 </li>
             @else
-                <li>
+                <li class="flex px-3 py-2 text-sm font-semibold text-gray-700 bg-gray-100 border border-r-0 border-gray-300 rounded-l hover:text-gray-800 hover:bg-gray-50 focus:outline-none focus:shadow-outline">
                     <a href="#"
                        wire:click.prevent="previousPage"
                        rel="prev"
-                       class="block px-3 py-2 text-sm font-semibold text-gray-900 border border-r-0 border-gray-300 rounded-l hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline"
-                       aria-label="@lang('pagination.previous')">&larr;</a>
+                       class="self-center"
+                       aria-label="@lang('pagination.previous')">
+                        <x-heroicon-o-arrow-left class="w-4 h-4 text-gray-600" />
+                    </a>
                 </li>
             @endif
 
@@ -20,8 +24,8 @@
             @foreach ($elements as $element)
                 {{-- "Three Dots" Separator --}}
                 @if (is_string($element))
-                    <li aria-disabled="true">
-                        <span class="block px-3 py-2 text-gray-500 border border-r-0 border-gray-300">{{ $element }}</span>
+                    <li class="flex flex-1 px-3 py-2 text-gray-500 border border-r-0 border-gray-300" aria-disabled="true">
+                        <span>{{ $element }}</span>
                     </li>
                 @endif
 
@@ -29,14 +33,14 @@
                 @if (is_array($element))
                     @foreach ($element as $page => $url)
                         @if ($page == $paginator->currentPage())
-                            <li aria-current="page">
-                                <span class="block px-3 py-2 text-sm font-semibold text-white bg-gray-700 border border-r-0 border-gray-700">{{ $page }}</span>
+                            <li class="flex justify-center w-8 py-2 text-sm font-semibold text-center text-white bg-blue-600 border border-r-0 border-blue-600" aria-current="page">
+                                <span class="self-center">{{ $page }}</span>
                             </li>
                         @else
-                            <li>
+                            <li class="flex justify-center w-8 py-2 text-sm font-semibold text-center text-gray-700 bg-gray-100 border border-r-0 border-gray-300 hover:text-gray-800 hover:bg-gray-50 focus:outline-none focus:shadow-outline">
                                 <a href="#"
                                    wire:click.prevent="gotoPage({{ $page }})"
-                                   class="block px-3 py-2 text-sm font-semibold text-gray-700 bg-gray-100 border border-r-0 border-gray-300 hover:text-gray-800 hover:bg-gray-50 focus:outline-none focus:shadow-outline"
+                                   class="self-center"
                                    aria-label="@lang('pagination.goto_page', ['page' => $page])">{{ $page }}</a>
                             </li>
                         @endif
@@ -46,16 +50,20 @@
 
             {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
-                <li>
+                <li class="flex px-3 py-2 bg-gray-100 border border-gray-300 rounded-r hover:text-gray-800 hover:bg-gray-50 focus:outline-none focus:shadow-outline">
                     <a href="#"
                        wire:click.prevent="nextPage"
                        rel="next"
-                       class="block px-3 py-2 text-sm font-semibold text-gray-700 bg-gray-100 border border-gray-300 hover:text-gray-800 hover:bg-gray-50 focus:outline-none focus:shadow-outline"
-                       aria-label="@lang('pagination.next')">&rarr;</a>
+                       class="self-center"
+                       aria-label="@lang('pagination.next')">
+                        <x-heroicon-o-arrow-right class="w-4 h-4 text-gray-600" />
+                    </a>
                 </li>
             @else
-                <li aria-disabled="true" aria-label="@lang('pagination.next')">
-                    <span class="block px-3 py-2 text-sm font-semibold text-gray-500 border border-gray-300 rounded-r" aria-hidden="true">&rarr;</span>
+                <li class="flex px-3 py-2 border border-gray-300 rounded-r" aria-disabled="true" aria-label="@lang('pagination.next')">
+                    <span class="self-center" aria-hidden="true">
+                        <x-heroicon-o-arrow-right class="w-4 h-4 text-gray-400" />
+                    </span>
                 </li>
             @endif
         </ul>
